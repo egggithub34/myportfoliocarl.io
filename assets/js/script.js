cd63514d11472fa792088e7d85990c9f5cfb0e19
egggithub34 <150238194+egggithub34@users.noreply.github.com>
2023-11-08 20:01:59 +0800

    added new files

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const numbers = document.querySelectorAll(".number");
+const svgEl = document.querySelectorAll("svg circle");
+const counters = Array(numbers.length);
+const intervals = Array(counters.length);
+counters.fill(0);
+
+numbers.forEach((number, index) => {
+  intervals[index] = setInterval(() => {
+    if (counters[index] === parseInt(number.dataset.num)) {
+      clearInterval(intervals[index]); // Fix this line
+    } else {
+      counters[index] += 1;
+      number.innerHTML = counters[index] + "%";
+      svgEl[index].style.strokeDashoffset = Math.floor(
+        472 - 440 * parseFloat(number.dataset.num / 100)
+      );
+    }
+  }, 20);
+});
+
+const gobackbtn = document.querySelector(".gobackbtn");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    gobackbtn.classList.add("active");
+  } else {
+    gobackbtn.classList.remove("active");
+  }
+});
